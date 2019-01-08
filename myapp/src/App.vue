eec8600ba11b0b39ba60def6032d1076022c2a7a
yang <305741616@qq.com>
2019-01-08 17:53:06 +0800

    杨淇提交1

--- myapp/src/App.vue
+++ myapp/src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    <router-view/>
+    <router-view class="router"/>
     <Footer-com/>
   </div>
 </template>
@@ -15,9 +15,14 @@ export default {
 }
 </script>
 
-<style>
+<style scoper lang="scss">
 #app {
   width: 100%;
   height: 100%;
+  display: flex;
+  flex-direction: column;
+  .router{
+    flex:1;
+  }
 }
 </style>
