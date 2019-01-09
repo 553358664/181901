9cf1149c7db6903a62a926e4bf7e8d38902cfbc0
Lemon-mn <MRcastle@163.com>
2019-01-09 13:34:47 +0800

    liuna

--- myapp/src/components/my/details/components/header.vue
+++ myapp/src/components/my/details/components/header.vue
@@ -1,17 +1,46 @@
 <template>
     <div class="header">
-        详细信息
+        <img class="back" :src="back" @click="handleback()"/> 
+        <p>详细资料</p>
+        <p>...</p>  
     </div>
 </template>
 
 <script>
 export default {
-    
+    methods: {
+        handleback(){
+            this.$router.back();
+        }
+    },
+     data(){
+       return{
+           back:require('@/assets/my/icon_wd_jt@2x.png'),
+       }
+   }
 }
 </script>
 
 <style lang="scss" scoped>
-
+     .header{
+        width:100%;
+        height:.98rem;
+        display:flex;
+        flex-direction: row;
+        justify-content: space-between;
+        align-items: center;
+        background:#FCFCFA;
+        padding:0 .24rem;
+        margin-bottom: .2rem;
+    }
+    .back{
+        width:.34rem;
+        height:.34rem;
+    }
+    p{
+        font-size: .32rem;
+        color:#0A0A0A ;
+    }
 </style>
 
 
