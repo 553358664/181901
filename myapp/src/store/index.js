0669b51998605e5ee86df4baeff523a2323256a2
leegeeya <1138450193@qq.com>
2019-01-10 21:17:18 +0800

    子文件主分支第二次冲突

--- myapp/src/store/index.js
+++ myapp/src/store/index.js
@@ -1,21 +1,21 @@
 import Vue from "vue";
 import Vuex from "vuex";
 import community from "./community";
-<<<<<<< HEAD
+
 import register_login from "./register_login";
-=======
+
 import small from "./small";
->>>>>>> dd5954cd1a63568edea06cf789d757bf5f739551
+
 Vue.use(Vuex);
 
 const store = new Vuex.Store({
     modules:{
         community,
-<<<<<<< HEAD
-        register_login
-=======
+
+        register_login,
+
         small
->>>>>>> dd5954cd1a63568edea06cf789d757bf5f739551
+
     }
 })
 
