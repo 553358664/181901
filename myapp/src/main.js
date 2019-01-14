625ec354e12ebd3fe2709f68e8ece5c3b2add466
qiu <18240395386@163.com>
2019-01-14 16:33:22 +0800

    err

--- myapp/src/main.js
+++ myapp/src/main.js
@@ -4,8 +4,6 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import store from "./store";
-//拖拽组件
-// import VueDnd from "awe-dnd"
 //修改全局mint-ui的scss文件
 import "./common/css/my-mint.scss";
 
@@ -16,7 +14,6 @@ import "./common/js/flexble";
 // import "../node_modules/touchjs/dist/touch-0.2.14.js"
 // Vue.config.productionTip = false
 // Vue.use(VueDnd)
-
 
 
 
